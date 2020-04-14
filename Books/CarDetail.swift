4e79b873ecea70132b34d59f38f5d2dce1c141b7
TsuNiWan <tom007880510@gmail.com>
2020-04-14 19:59:02 +0800

    Fix text not display

--- Books/CarDetail.swift
+++ Books/CarDetail.swift
@@ -34,7 +34,7 @@ struct CarDetail: View {
                         .hidden()
                 }
             }
-            .animation(.easeInOut(duration: 5))
+            .animation(.easeInOut(duration: 3))
             .onAppear{
                 self.show = true
             }
@@ -43,16 +43,29 @@ struct CarDetail: View {
                 Text("售價：").font(.custom("jf-openhuninn-1.0", size: 18))  +
                     Text(car.price)
                         .font(.title).fontWeight(.heavy).foregroundColor(.red).underline()
+                
                 Group {
                     Text("全長(mm)：").font(.custom("jf-openhuninn-1.0", size: 18))  + Text(car.length).font(.title).fontWeight(.heavy).italic()
                     Text("全寬(mm)：").font(.custom("jf-openhuninn-1.0", size: 18))  + Text(car.width)
-                    .font(.title).fontWeight(.heavy).italic()
+                        .font(.title).fontWeight(.heavy).italic()
                     Text("全高(mm)：").font(.custom("jf-openhuninn-1.0", size: 18)) + Text(car.height).font(.title).fontWeight(.heavy).italic()
                 }
+                
                 Group {
                     Text("排氣量(c.c.)：").font(.custom("jf-openhuninn-1.0", size: 18))  + Text(car.displacement).font(.title).fontWeight(.heavy).foregroundColor(.orange).underline()
-                    Text("最大馬力(ps/rpm)：").font(.custom("jf-openhuninn-1.0", size: 18))  + Text(car.horsepower).font(.title).fontWeight(.heavy).italic()
-                    Text("最打扭力(kg-m/rpm)：").font(.custom("jf-openhuninn-1.0", size: 18))  + Text(car.torque).font(.title).fontWeight(.heavy).italic()
+                    Text("最大馬力(ps/rpm)：").font(.custom("jf-openhuninn-1.0", size: 18))
+                        .padding(.top, 5)
+                    Text(car.horsepower).font(.title).fontWeight(.heavy).italic().padding(EdgeInsets(top: 2, leading: 10, bottom: 10, trailing: 10))
+                    Text("最大扭力(kg-m/rpm)：").font(.custom("jf-openhuninn-1.0", size: 18))
+                    .padding(.top, 5)
+                    Text(car.torque).font(.title).fontWeight(.heavy).italic()
+                        .padding(EdgeInsets(top: 2, leading: 10, bottom: 10, trailing: 10))
+                }
+                
+                if car.type == "油電複合車" || car.type == "插電式油電複合車"{
+                    Text("括號內為電動馬達")
+                        .font(.caption)
+                        .padding(.top, 10)
                 }
             }
         }
@@ -63,6 +76,6 @@ struct CarDetail: View {
 
 struct CarDetail_Previews: PreviewProvider {
     static var previews: some View {
-        CarDetail(car: cars[0])
+        CarDetail(car: hybrid[0])
     }
 }
